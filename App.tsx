0a038e426caff06757c1846361669bb2700f67a6
MZotti <marcoshzotti@gmail.com>
2023-02-25 21:47:57 -0300

    fix: splash screen and adaptive icon

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { NativeBaseProvider, StorageManager, ColorMode } from "native-base";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import WeatherQueryProvider from "@providers/weatherQueryProvider"
 
-import SplashScreen from "@views/SplashScreen";
 import Navigator from "./src/Navigator";
 
 const colorModeManager: StorageManager = {
@@ -27,18 +26,12 @@ const colorModeManager: StorageManager = {
 };
 
 export default function App() {
-    const [appLoaded, setAppLoaded] = useState(false)
-
     return (
         <NativeBaseProvider colorModeManager={colorModeManager}>
             <SafeAreaView style={styles.container}>
-                {
-                    !appLoaded
-                        ? <SplashScreen setAppLoaded={setAppLoaded} />
-                        : <WeatherQueryProvider>
-                            <Navigator />
-                        </WeatherQueryProvider>
-                }
+                <WeatherQueryProvider>
+                    <Navigator />
+                </WeatherQueryProvider>
             </SafeAreaView>
         </NativeBaseProvider>
     );
